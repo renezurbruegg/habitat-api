64cf46ec11e6e461ed22e851e9485e935a949e4c
brucec <brucec@hotmail.com>
2019-05-16 19:23:57 -0700

    add update attitude function to example.py

--- examples/example.py
+++ examples/example.py
@@ -7,6 +7,7 @@
 import habitat
 import pickle
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def example():
@@ -29,16 +30,44 @@ def example():
 
         ax=env._sim._sim.agents[0].scene_node.absolute_transformation()[0:3, _x_axis]
         env._sim._sim.agents[0].scene_node.translate_local(ax * vx*dt)
+
+    def update_attitude(roll,pitch,yaw,dt):
+
+        ax_roll = np.zeros(3, dtype=np.float32)
+        ax_roll[_z_axis] = 1
+        env._sim._sim.agents[0].scene_node.rotate_local(np.deg2rad(roll*dt), ax_roll)
+        env._sim._sim.agents[0].scene_node.normalize()
+        
+        ax_pitch = np.zeros(3, dtype=np.float32)
+        ax_pitch[_x_axis] = 1
+        env._sim._sim.agents[0].scene_node.rotate_local(np.deg2rad(pitch*dt), ax_pitch)
+        env._sim._sim.agents[0].scene_node.normalize()
+
+        ax_yaw = np.zeros(3, dtype=np.float32)
+        ax_yaw[_y_axis] = 1
+        env._sim._sim.agents[0].scene_node.rotate_local(np.deg2rad(yaw*dt), ax_yaw)
+        env._sim._sim.agents[0].scene_node.normalize()
+
+
+
+        
     
     while not env.episode_over:
         #observations = env.step(env.action_space.sample())
         
         #ax=env._sim._sim.agents[0].scene_node.absolute_transformation()[0:3, _z_axis]
         #env._sim._sim.agents[0].scene_node.translate_local(ax * 0.8)
-        
         #observations = env.step(3)
+        
+        #update_position(-1,0,1)
+        update_attitude(0,0,30,1)
 
-        update_position(-1,0,1)
+        #ax = np.zeros(3, dtype=np.float32)
+        #ax[_y_axis] = 1
+
+        #env._sim._sim.agents[0].scene_node.rotate_local(np.deg2rad(30), ax)
+        #env._sim._sim.agents[0].scene_node.normalize()
+
 
         sim_obs=env._sim._sim.get_sensor_observations()
         observations = env._sim._sensor_suite.get_observations(sim_obs)
