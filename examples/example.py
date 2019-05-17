f32d54047fc4880a5c5ce028ece7d5ecc1a6d08c
brucec <brucec@hotmail.com>
2019-05-16 17:34:46 -0700

    add update position function in example.py

--- examples/example.py
+++ examples/example.py
@@ -22,16 +22,24 @@ def example():
     _x_axis = 0
     _y_axis = 1
     _z_axis = 2
+
+    def update_position(vz,vx,dt):
+        ax=env._sim._sim.agents[0].scene_node.absolute_transformation()[0:3, _z_axis]
+        env._sim._sim.agents[0].scene_node.translate_local(ax * vz*dt)
+
+        ax=env._sim._sim.agents[0].scene_node.absolute_transformation()[0:3, _x_axis]
+        env._sim._sim.agents[0].scene_node.translate_local(ax * vx*dt)
     
     while not env.episode_over:
         #observations = env.step(env.action_space.sample())
-
-        #ax=env._sim._sim.agents[0].scene_node.absolute_transformation()[0:3, 2]
-        #env._sim._sim.agents[0].scene_node.translate_local(ax * 0.05)
-        ax=env._sim._sim.agents[0].scene_node.absolute_transformation()[0:3, _x_axis]
-        env._sim._sim.agents[0].scene_node.translate_local(ax * 0.8)
+        
+        #ax=env._sim._sim.agents[0].scene_node.absolute_transformation()[0:3, _z_axis]
+        #env._sim._sim.agents[0].scene_node.translate_local(ax * 0.8)
         
         #observations = env.step(3)
+
+        update_position(-1,0,1)
+
         sim_obs=env._sim._sim.get_sensor_observations()
         observations = env._sim._sensor_suite.get_observations(sim_obs)
         plt.imshow(observations['rgb'])
