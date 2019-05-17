867428342c0d4e6c63e085bfcdd5146af5cc3096
brucec <brucec@hotmail.com>
2019-05-16 17:26:30 -0700

    add axis definitions to example.py

--- examples/example.py
+++ examples/example.py
@@ -18,25 +18,26 @@ def example():
     
     print("Agent stepping around inside environment.")
     count_steps = 0
+
+    _x_axis = 0
+    _y_axis = 1
+    _z_axis = 2
+    
     while not env.episode_over:
         #observations = env.step(env.action_space.sample())
 
-
-
         #ax=env._sim._sim.agents[0].scene_node.absolute_transformation()[0:3, 2]
         #env._sim._sim.agents[0].scene_node.translate_local(ax * 0.05)
-        ax=env._sim._sim.agents[0].scene_node.absolute_transformation()[0:3, 0]
+        ax=env._sim._sim.agents[0].scene_node.absolute_transformation()[0:3, _x_axis]
         env._sim._sim.agents[0].scene_node.translate_local(ax * 0.8)
         
-        
-
         #observations = env.step(3)
         sim_obs=env._sim._sim.get_sensor_observations()
         observations = env._sim._sensor_suite.get_observations(sim_obs)
         plt.imshow(observations['rgb'])
         plt.show()
-
         count_steps += 1
+
     print("Episode finished after {} steps.".format(count_steps))
     
     #for determining what observation and action spaces are
