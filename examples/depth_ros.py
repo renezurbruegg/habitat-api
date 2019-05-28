922221760ec3725efb57980132dd45ade08ce334
brucec <brucec@hotmail.com>
2019-05-28 16:25:57 -0700

    add needed image and camera info publishers for depthimage_to_laserscan

--- examples/depth_ros.py
+++ examples/depth_ros.py
@@ -10,22 +10,42 @@ from rospy_tutorials.msg import Floats
 from rospy.numpy_msg import numpy_msg
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 from cv_bridge import CvBridge, CvBridgeError
 
 
 pub = rospy.Publisher("ros_img_depth", Image, queue_size=10)
-
+camera_info_pub = rospy.Publisher("camera_info_topic", CameraInfo, queue_size=0)
 
 def callback(data):
     print(rospy.get_name(), "I heard %s" % str(data.data))
-    img = (np.reshape(data.data, (256, 256))).astype(np.uint8)
+    img = (np.reshape(data.data, (256, 256))).astype(np.uint16)
 
-    image_message = CvBridge().cv2_to_imgmsg(img, encoding="mono8")
+    image_message = CvBridge().cv2_to_imgmsg(img, encoding="mono16")
+
     pub.publish(image_message)
+
+    camera_info_msg = CameraInfo()
+    width, height = 256, 256
+    fx, fy = 0, 0
+    cx, cy = 0, 0
+    camera_info_msg.width = width
+    camera_info_msg.height = height
+    camera_info_msg.distortion_model = "plumb_bob"
+    camera_info_msg.K = [fx, 0, cx,
+                            0, fy, cy,
+                            0, 0, 1]
+                            
+    camera_info_msg.D = [0, 0, 0, 0,0]
+
+    camera_info_msg.P = [fx, 0, cx, 0,
+                            0, fy, cy, 0,
+                            0, 0, 1, 0]
+                            
+    camera_info_pub.publish(camera_info_msg)
 
 
 def listener():
