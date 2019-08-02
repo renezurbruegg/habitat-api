c335b275ce9b9a62acc07cc70fb40e68c2261e71
brucec <brucec@hotmail.com>
2019-08-01 17:15:25 -0700

    add get ground truth  ros map and yaml function

---
+++ habitat_ros/get_ros_map.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import os
+
+import imageio
+import numpy as np
+
+import habitat
+from habitat.tasks.nav.nav_task import NavigationEpisode, NavigationGoal
+from habitat.utils.visualizations import maps
+from habitat.utils.visualizations.maps import COORDINATE_MIN, COORDINATE_MAX
+
+MAP_DIR = os.path.join("habitat_ros", "maps")
+if not os.path.exists(MAP_DIR):
+    os.makedirs(MAP_DIR)
+
+def get_topdown_map(config_paths, map_name):
+
+    config = habitat.get_config(config_paths=config_paths)
+    dataset = habitat.make_dataset(
+        id_dataset=config.DATASET.TYPE, config=config.DATASET
+    )
+    env = habitat.Env(config=config, dataset=dataset)
+    env.reset()
+    top_down_map = maps.get_topdown_map(env.sim, map_resolution=(2048, 2048))
+
+    # Image containing 0 if occupied, 1 if unoccupied, and 2 if border (if
+    # the flag is set)
+    top_down_map[np.where(top_down_map == 0)] = 125
+    top_down_map[np.where(top_down_map == 1)] = 255
+    top_down_map[np.where(top_down_map == 2)] = 0
+
+    imageio.imsave(os.path.join(MAP_DIR, map_name + ".pgm"), top_down_map)
+
+    complete_name = os.path.join(MAP_DIR, map_name + ".yaml")
+    f = open(complete_name, "w+")
+
+    f.write("image: " + map_name + ".pgm\n")
+    f.write("resolution:" + str((COORDINATE_MAX - COORDINATE_MIN) / 2048) + "\n")
+    f.write("origin: [" + str(-1) + "," + str(-1) + ", 0.000000]\n")
+    f.write("negate: 0\noccupied_thresh: 0.65\nfree_thresh: 0.196")
+    f.close()
+
+
+def main():
+    get_topdown_map("configs/tasks/pointnav_rgbd_gibson.yaml", "default")
+
+
+if __name__ == "__main__":
+    main()
