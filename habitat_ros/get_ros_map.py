1465720eefd9db843020581ecbcf72f3747b49fe
brucec <brucec@hotmail.com>
2019-08-07 18:10:24 -0700

    add comment to get_ros map

--- habitat_ros/get_ros_map.py
+++ habitat_ros/get_ros_map.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+#This script geneartes a ros/rviz compatible map based on the specified Habitat scene's top-down map
 
 import os
 import argparse
