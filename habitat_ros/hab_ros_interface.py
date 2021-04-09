86af1a15ce4ea4d81d30e7991864e50b9fcb6ee2
René Zurbrügg <zrene@student.ethz.ch>
2021-04-09 15:51:51 +0200

    minor changes

--- habitat_ros/hab_ros_interface.py
+++ habitat_ros/hab_ros_interface.py
@@ -4,8 +4,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from gym import spaces
 
 import rospy
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 from rospy.numpy_msg import numpy_msg
 from rospy_tutorials.msg import Floats
 from geometry_msgs.msg import Twist
@@ -26,7 +30,6 @@ import cv2
 lock = threading.Lock()
 rospy.init_node("habitat", anonymous=False)
 
-
 class sim_env(threading.Thread):
 
     _x_axis = 0
@@ -36,23 +39,29 @@ class sim_env(threading.Thread):
     _sensor_rate = 50  # hz
     _r = rospy.Rate(_sensor_rate)
 
-    def __init__(self, env_config_file):
+    def __init__(self, cfg):
         threading.Thread.__init__(self)
-        self.env = habitat.Env(config=habitat.get_config(env_config_file))
+        self.env = habitat.Env(config=cfg)
         # always assume height equals width
         self._sensor_resolution = {
             "RGB": self.env._sim.config["RGB_SENSOR"]["HEIGHT"],
             "DEPTH": self.env._sim.config["DEPTH_SENSOR"]["HEIGHT"],
             "BC_SENSOR": self.env._sim.config["BC_SENSOR"]["HEIGHT"],
+            "SEMANTIC": self.env._sim.config["SEMANTIC_SENSOR"]["HEIGHT"],
         }
-        self.env._sim._sim.agents[0].move_filter_fn = self.env._sim._sim._step_filter
+
+
+        #import pdb; pdb.set_trace();
+        self.env._sim._sim.agents[0].move_filter_fn = self.env._sim._sim.step_filter
         self.observations = self.env.reset()
 
         self.env._sim._sim.agents[0].state.velocity = np.float32([0, 0, 0])
         self.env._sim._sim.agents[0].state.angular_velocity = np.float32([0, 0, 0])
 
-        self._pub_rgb = rospy.Publisher("~rgb", numpy_msg(Floats), queue_size=1)
-        self._pub_depth = rospy.Publisher("~depth", numpy_msg(Floats), queue_size=1)
+        self._pub_rgb = rospy.Publisher("~rgb", Image, queue_size=1)
+        self._pub_depth = rospy.Publisher("~depth", Image, queue_size=1)
+        self._pub_semantic_sensor = rospy.Publisher("~semantic", Image, queue_size=1)
+
 
         # additional RGB sensor I configured
         self._pub_bc_sensor = rospy.Publisher(
@@ -129,27 +138,64 @@ class sim_env(threading.Thread):
         while not rospy.is_shutdown():
             lock.acquire()
 
-            rgb_with_res = np.concatenate(
-                (
-                    np.float32(self.observations["rgb"].ravel()),
-                    np.array(
-                        [self._sensor_resolution["RGB"], self._sensor_resolution["RGB"]]
-                    ),
-                )
-            )
+            # rgb_with_res = np.concatenate(
+            #     (
+            #         np.float32(self.observations["rgb"].ravel()),
+            #         np.array(
+            #             [self._sensor_resolution["RGB"], self._sensor_resolution["RGB"]]
+            #         ),
+            #     )
+            # )
+            #
 
-            # multiply by 10 to get distance in meters
-            depth_with_res = np.concatenate(
-                (
-                    np.float32(self.observations["depth"].ravel() * 10),
-                    np.array(
-                        [
-                            self._sensor_resolution["DEPTH"],
-                            self._sensor_resolution["DEPTH"],
-                        ]
-                    ),
-                )
-            )
+            rgb_msg = Image()
+            rgb_msg.height = self._sensor_resolution["RGB"]
+            rgb_msg.width = self._sensor_resolution["RGB"]
+            rgb_msg.data = self.observations["rgb"].flatten().tolist()
+            rgb_msg.encoding = "rgb8"
+            #
+            # # multiply by 10 to get distance in meters
+            # depth_with_res = np.concatenate(
+            #     (
+            #         np.float32(self.observations["depth"].ravel() * 10),
+            #         np.array(
+            #             [
+            #                 self._sensor_resolution["DEPTH"],
+            #                 self._sensor_resolution["DEPTH"],
+            #             ]
+            #         ),
+            #     )
+            # )
+
+            depth_msg = Image()
+            depth_msg.height = self._sensor_resolution["DEPTH"]
+            depth_msg.width = self._sensor_resolution["DEPTH"]
+            maxDistance = 5 #m
+            depth_msg.data = np.uint8((self.observations["depth"].ravel() * 10) * 255/maxDistance).flatten().tolist()
+            depth_msg.encoding = "mono8"
+
+
+
+            # Create and publish image message
+            img_msg = Image()
+            img_msg.height = self._sensor_resolution["SEMANTIC"]
+            img_msg.width = self._sensor_resolution["SEMANTIC"]
+            img_msg.data = self.observations["semantic"].flatten().tolist()
+            img_msg.encoding = "mono8"
+            # img_pub.publish(img_msg)
+
+            # semantic_with_res = np.concatenate(
+            #     (
+            #         np.uint32(self.observations["semantic"].ravel()),
+            #         np.array(
+            #             [
+            #                 self._sensor_resolution["SEMANTIC"],
+            #                 self._sensor_resolution["SEMANTIC"],
+            #             ]
+            #         ),
+            #     )
+            # )
+
 
             bc_sensor_with_res = np.concatenate(
                 (
@@ -167,9 +213,12 @@ class sim_env(threading.Thread):
             pointgoal_np = np.float32(self.observations["pointgoal"].ravel())
             lock.release()
 
-            self._pub_rgb.publish(np.float32(rgb_with_res))
-            self._pub_depth.publish(np.float32(depth_with_res))
+            self._pub_rgb.publish(rgb_msg)
+            self._pub_depth.publish(depth_msg)
             self._pub_bc_sensor.publish(np.float32(bc_sensor_with_res))
+
+            self._pub_semantic_sensor.publish(img_msg)
+
 
             depth_pointgoal_np = np.concatenate((depth_np, pointgoal_np))
             self._pub_depth_and_pointgoal.publish(np.float32(depth_pointgoal_np))
@@ -200,9 +249,98 @@ def callback(vel, my_env):
     lock.release()
 
 
-def main():
 
-    my_env = sim_env(env_config_file="configs/tasks/pointnav_rgbd.yaml")
+
+# Define the sensor and register it with habitat
+# For the sensor, we will register it with a custom name
+@habitat.registry.register_sensor(name="my_supercool_sensor")
+class AgentPositionSensor(habitat.Sensor):
+    def __init__(self, sim, config, **kwargs):
+        super().__init__(config=config)
+        self._sim = sim
+
+    # Defines the name of the sensor in the sensor suite dictionary
+    def _get_uuid(self, *args, **kwargs):
+        return "agent_position"
+
+    # Defines the type of the sensor
+    def _get_sensor_type(self, *args, **kwargs):
+        return habitat.SensorTypes.POSITION
+
+    # Defines the size and range of the observations of the sensor
+    def _get_observation_space(self, *args, **kwargs):
+        return spaces.Box(
+            low=np.finfo(np.float32).min,
+            high=np.finfo(np.float32).max,
+            shape=(3,),
+            dtype=np.float32,
+        )
+
+    # This is called whenver reset is called or an action is taken
+    def get_observation(self, observations, episode):
+        return self._sim.get_agent_state().position
+
+
+# Define the sensor and register it with habitat
+# For the sensor, we will register it with a custom name
+@habitat.registry.register_sensor(name="semantic_segmentation_mask")
+class SemanticSegmentationMaskSensor(habitat.Sensor):
+    def __init__(self, sim, config, **kwargs):
+        super().__init__(config=config)
+        self._sim = sim
+
+    # Defines the name of the sensor in the sensor suite dictionary
+    def _get_uuid(self, *args, **kwargs):
+        return "semantic_segmentation_mask"
+
+    # Defines the type of the sensor
+    def _get_sensor_type(self, *args, **kwargs):
+        return habitat.SensorTypes.POSITION
+
+    # Defines the size and range of the observations of the sensor
+    def _get_observation_space(self, *args, **kwargs):
+        return spaces.Box(
+            low=np.finfo(np.float32).min,
+            high=np.finfo(np.float32).max,
+            shape=(3,),
+            dtype=np.float32,
+        )
+
+    # This is called whenver reset is called or an action is taken
+    def get_observation(self, observations, episode):
+        return self._sim.get_agent_state().position
+
+
+def main():
+    config = habitat.get_config(config_paths="/home/rene/catkin_ws/src/habitat-api/configs/tasks/pointnav_rgbd.yaml")
+
+    config.defrost()
+
+    # Add things to the config to for the measure
+    config.TASK.EPISODE_INFO = habitat.Config()
+    # The type field is used to look-up the measure in the registry.
+    # By default, the things are registered with the class name
+    config.TASK.EPISODE_INFO.TYPE = "EpisodeInfo"
+    config.TASK.EPISODE_INFO.VALUE = 5
+
+    # Now define the config for the sensor
+    config.TASK.AGENT_POSITION_SENSOR = habitat.Config()
+    # Use the custom name
+    config.TASK.AGENT_POSITION_SENSOR.TYPE = "my_supercool_sensor"
+    # Add the sensor to the list of sensors in use
+    config.TASK.SENSORS.append("AGENT_POSITION_SENSOR")
+
+
+    # Now define the config for the sensor
+    config.TASK.SEMANTIC_SEGMENTATION_MASK_SENSOR = habitat.Config()
+    # Use the custom name
+    config.TASK.SEMANTIC_SEGMENTATION_MASK_SENSOR.TYPE = "semantic_segmentation_mask"
+    # Add the sensor to the list of sensors in use
+    config.TASK.SENSORS.append("SEMANTIC_SEGMENTATION_MASK_SENSOR")
+
+    config.freeze()
+
+    my_env = sim_env(config)
     # start the thread that publishes sensor readings
     my_env.start()
 
